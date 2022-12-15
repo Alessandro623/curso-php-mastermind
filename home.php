74b43327b260cf2cff6d64bd763da24ee2ae9905
Alessandro623 <ordnasela_2010@hotmail.com.ar>
2022-12-14 23:34:36 -0300

    User-contacts relation

--- home.php
+++ home.php
@@ -7,7 +7,7 @@
     return;
   }
   
-  $contacts = $conn->query("SELECT * FROM contacts");
+  $contacts = $conn->query("SELECT * FROM contacts WHERE user_id = {$_SESSION["user"]["id"]}");
 ?>
 
 <?php require "partial/header.php" ?>
